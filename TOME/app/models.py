9026c098d6f35f0d914b8d2f8b566bfce86ebf2d
Kor 0 <40504329+kor0@users.noreply.github.com>
2018-07-03 16:32:18 -0300

    Update models.py

--- TOME/app/models.py
+++ TOME/app/models.py
@@ -1,19 +1,37 @@
 from mongoengine import Document
 from mongoengine import DateTimeField, StringField, ReferenceField, ListField, IntField
+from flask_appbuilder.security.mongoengine.manager import SecurityManager
+from flask_appbuilder.security.mongoengine.models import User
 
 """
 
 Define you MongoEngine Models here
 
 """
-
 class Team(Document):
     name = StringField(max_length = 60, required = True, unique = True)
+    desc = StringField(max_length = 60, required = True, unique = True)
 
     def __unicode__(self):
         return self.name
 
     def __repr__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
+class Proj(Document):
+    name = StringField(max_length = 60, required = True, unique = True)
+    desc = StringField(max_length = 60, required = True, unique = True)
+
+    def __unicode__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
+    def __str__(self):
         return self.name
 
 class Gender(Document):
@@ -26,12 +44,6 @@ class Gender(Document):
         return self.name
 
     def __str__(self):
-        return self.name
-
-class Tags(Document):
-    name = StringField(max_length=60, required=True, unique=True)
-
-    def __unicode__(self):
         return self.name
 
 class Languages(Document):
@@ -70,18 +82,23 @@ class Action(Document):
     def __str__(self):
         return self.name
 
+
 class Employee(Document):
-    name = StringField(max_length=60, required=True, unique=True)
-    address = StringField(max_length=60)
+    user = ReferenceField(User, required=True, unique=True)
+    name = StringField(max_length=60)
     personal_phone = StringField(max_length=20)
     personal_celphone = StringField(max_length=20)
-    team = ReferenceField(Team, required=True)
+    team = ReferenceField(Team)
     gender = ReferenceField(Gender, required=True)
-    tags = ListField(ReferenceField(Tags))
-    prefered_language = ReferenceField(Languages, required = True)
+    prefered_language = ListField(ReferenceField(Languages, required = True))
     prefered_area = ReferenceField(Stack, required = True)
-    prefered_activity = ReferenceField(Action, required = True)
+    prefered_activity = ListField(ReferenceField(Action, required = True))
     years_experience = IntField(required=True)
 
-
+class Client(Document):
+    user = ReferenceField(User, required=True, unique=True)
+    name = StringField(max_length=60, required=True, unique=True)
+    personal_phone = StringField(max_length=20)
+    personal_celphone = StringField(max_length=20)
+    project = ReferenceField(Proj)
 
