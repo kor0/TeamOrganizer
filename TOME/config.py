18e43f4cd690ba85d42b61fc66ee5f06fe40a1ff
Kor 0 <40504329+kor0@users.noreply.github.com>
2018-07-03 16:31:47 -0300

    Update config.py

--- TOME/config.py
+++ TOME/config.py
@@ -2,64 +2,32 @@ import os
 from flask_appbuilder.security.manager import AUTH_OID, AUTH_REMOTE_USER, AUTH_DB, AUTH_LDAP
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# Your App secret key
+# App secret key
 SECRET_KEY = '\2\1thisismyscretkey\1\2\e\y\y\h'
 
 # The MongoEngine connection string.
-MONGODB_SETTINGS = {'DB': 'mydb'}
+MONGODB_SETTINGS = {'DB': 'mydb11'}
 
 # Flask-WTF flag for CSRF
 CSRF_ENABLED = True
 
-#------------------------------
-# GLOBALS FOR APP Builder 
-#------------------------------
-# Uncomment to setup Your App name
-#APP_NAME = "My App Name"
-
-# Uncomment to setup Setup an App icon
-#APP_ICON = "static/img/logo.jpg"
+APP_NAME = "TOME"
 
 #----------------------------------------------------
 # AUTHENTICATION CONFIG
 #----------------------------------------------------
 # The authentication type
-# AUTH_OID : Is for OpenID
 # AUTH_DB : Is for database (username/password()
-# AUTH_LDAP : Is for LDAP
-# AUTH_REMOTE_USER : Is for using REMOTE_USER from web server
 AUTH_TYPE = AUTH_DB
 
-# Uncomment to setup Full admin role name
-#AUTH_ROLE_ADMIN = 'Admin'
-
-# Uncomment to setup Public role name, no authentication needed
-#AUTH_ROLE_PUBLIC = 'Public'
-
-# Will allow user self registration
-#AUTH_USER_REGISTRATION = True
-
-# The default user self registration role
-#AUTH_USER_REGISTRATION_ROLE = "Public"
-
-# When using LDAP Auth, setup the ldap server
-#AUTH_LDAP_SERVER = "ldap://ldapserver.new"
-
-# Uncomment to setup OpenID providers example for OpenID authentication
-#OPENID_PROVIDERS = [
-#    { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
-#    { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
-#    { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
-#    { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
-#    { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
 #---------------------------------------------------
 # Babel config for translations
 #---------------------------------------------------
 # Setup default language
 BABEL_DEFAULT_LOCALE = 'en'
-# Your application default translation path
+# Application default translation path
 BABEL_DEFAULT_FOLDER = 'translations'
-# The allowed translation for you app
+# The allowed translation for the app
 LANGUAGES = {
     'en': {'flag':'gb', 'name':'English'},
     'pt': {'flag':'pt', 'name':'Portuguese'},
@@ -69,6 +37,7 @@ LANGUAGES = {
     'zh': {'flag':'cn', 'name':'Chinese'},
     'ru': {'flag':'ru', 'name':'Russian'}
 }
+
 #---------------------------------------------------
 # Image and file configuration
 #---------------------------------------------------
@@ -85,18 +54,4 @@ IMG_UPLOAD_URL = '/static/uploads/'
 
 # Theme configuration
 # these are located on static/appbuilder/css/themes
-# you can create your own and easily use them placing them on the same dir structure to override
-#APP_THEME = "bootstrap-theme.css"  # default bootstrap
-#APP_THEME = "cerulean.css"
-#APP_THEME = "amelia.css"
-#APP_THEME = "cosmo.css"
-#APP_THEME = "cyborg.css"  
-#APP_THEME = "flatly.css"
-#APP_THEME = "journal.css"
-#APP_THEME = "readable.css"
-#APP_THEME = "simplex.css"
-#APP_THEME = "slate.css"   
-#APP_THEME = "spacelab.css"
-#APP_THEME = "united.css"
-#APP_THEME = "yeti.css"
-
+APP_THEME = "readable.css"
