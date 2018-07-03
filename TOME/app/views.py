5c7d7e964a3b8e791d18dd7faa7ecde6289a6791
Kor 0 <40504329+kor0@users.noreply.github.com>
2018-07-03 16:31:23 -0300

    Update views.py

--- TOME/app/views.py
+++ TOME/app/views.py
@@ -2,38 +2,41 @@ from flask import render_template
 from flask_appbuilder import expose, has_access
 from flask_appbuilder import ModelView
 from flask_appbuilder.models.mongoengine.interface import MongoEngineInterface
-from app import appbuilder
-from .models import Team, Employee, Tags, Gender
-"""
-    Define you Views here
-"""
+from app import appbuilder, db
+from .models import Team, Employee, Gender, Proj, Client, Stack, Action, Languages
 
+"""
+Views 
+"""
 class EmployeeModelView(ModelView):
     datamodel = MongoEngineInterface(Employee)
 
     label_columns = {'team':'Team'}
-    list_columns = ['name','personal_celphone','team']
+    list_columns = ['name','prefered_area','team']
 
     show_fieldsets = [
-        ('Summary',{'fields':['name','address','contact_group']}),
-        ('Personal Info',{'fields':['personal_phone','personal_celphone'],'expanded':False}),
+        ('Summary',{'fields':['user', 'team']}),
+        ('Assets',{'fields':['prefered_activity','prefered_language', 'prefered_area', 'years_experience'],'expanded':False}),
         ]
-appbuilder.add_view(EmployeeModelView, "List Employees",icon = "fa-envelope",category = "Teams")
+appbuilder.add_view(EmployeeModelView, "List Employees",category = "Employees")
+
+class ClientModelView(ModelView):
+    datamodel = MongoEngineInterface(Client)
+appbuilder.add_view(ClientModelView, "List Clients",category = "Clients")
+
+class ProjectModelView(ModelView):
+    datamodel = MongoEngineInterface(Proj)
+appbuilder.add_view(ProjectModelView, "List Projects",category = "Projects")
 
 class TeamModelView(ModelView):
     datamodel = MongoEngineInterface(Team)
     related_views = [EmployeeModelView]
-appbuilder.add_view(TeamModelView, "List Teams",icon = "fa-folder-open-o",category = "Teams",
-                category_icon = "fa-envelope")
+appbuilder.add_view(TeamModelView, "List Teams",category = "Teams")
 
-#class GenderView(ModelView):
-#    datamodel = MongoEngineInterface(Gender)
-#appbuilder.add_view(GenderView, "List Gender",icon = "fa-envelope",category = "Gender")
-
+appbuilder.security_cleanup()
 """
     Application wide 404 error handler
 """
 @appbuilder.app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html', base_template=appbuilder.base_template, appbuilder=appbuilder), 404
-
