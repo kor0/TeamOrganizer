b61644eaa38fc4429bde8fa115bf59a4fa65a557
Kor 0 <40504329+kor0@users.noreply.github.com>
2018-07-04 17:17:30 -0300

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 #!flask/bin/python
 from app import app
+import sys
 import os
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    #port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=sys.argv[1])
