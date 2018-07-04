e2fdca0efeb6b2b745c9c2b474c2dcd4d28724b2
Kor 0 <40504329+kor0@users.noreply.github.com>
2018-07-04 15:19:46 -0300

    Update config.py

--- config.py
+++ config.py
@@ -5,10 +5,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # App secret key
 SECRET_KEY = '\2\1thdbfhsdfgzgdshrshhsfcretkey\1\2\e\y\y\h'
 
+port = int(os.environ.get("PORT", 5000))
 # The MongoEngine connection string.
 MONGODB_SETTINGS = {
     'db': 'heroku_0dv5k10h', 
     'host': 'mongodb://heroku_0dv5k10h:9n9es8sdacme97kqt9duq59p86@ds127851.mlab.com:27851',
+    'port': port
 }
 
 # Flask-WTF flag for CSRF
