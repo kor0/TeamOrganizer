1cb30d7748ddcff0529644fe886f3cc3e37afcca
Kor 0 <40504329+kor0@users.noreply.github.com>
2018-07-04 15:59:15 -0300

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,4 @@
-import os
+
 from app import app
-
-port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port, debug=True)
-
+from os import environ
+app.run(debug=False, port=environ.get("PORT", 5000), processes=2)
