8d7035e7be62e1bcf215536e892ce875c79ec69f
Kor 0 <40504329+kor0@users.noreply.github.com>
2018-07-04 16:58:25 -0300

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from app import app
 from os import environ
-#port=environ.get("PORT")
-port = os.environ['PORT']
-app.run(debug=True, host=0.0.0.0, port=port)
+if __name__ == '__run__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
