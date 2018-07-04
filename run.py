3c614379b92ef1442a1f23bfec93b28f9460f7dd
Kor 0 <40504329+kor0@users.noreply.github.com>
2018-07-04 16:53:10 -0300

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from app import app
 from os import environ
 #port=environ.get("PORT")
-port = int(os.environ.get('PORT', 33507))
+port = os.environ['PORT']
 app.run(debug=True, host=0.0.0.0, port=port)
