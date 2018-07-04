1e302e8d663d9428cc151dc849e5e67408998059
Kor 0 <40504329+kor0@users.noreply.github.com>
2018-07-04 17:19:06 -0300

    Update run.py

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ import os
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     #port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=sys.argv[1])
+    app.run(host='0.0.0.0', port=int(sys.argv[1]))
