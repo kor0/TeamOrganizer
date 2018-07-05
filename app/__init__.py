4056115f6cd1d08100db7bbd2092f6eb53d19506
Kor 0 <40504329+kor0@users.noreply.github.com>
2018-07-05 17:17:13 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,16 +7,26 @@ from flask_mongoengine import MongoEngine
 """
  Logging configuration
 """
+class App:
+    class __App:
+        def __init__(self, arg):
+            self.val = arg
+        def __str__(self):
+            return repr(self) + self.val
+    instance = None
+    def __init__(self, arg):
+        if not App.instance:
+            App.instance = App.__OnlyOne(arg)
+            logging.basicConfig(format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+            logging.getLogger().setLevel(logging.DEBUG)
 
-logging.basicConfig(format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
-logging.getLogger().setLevel(logging.DEBUG)
-
-app = Flask(__name__)
-app.config.from_object('config')
-app.config['MONGODB_SETTINGS'] = {'HOST':'mongodb://careful:123456abc@ds127771.mlab.com:27771/careful','DB': 'careful'}
-db = MongoEngine(app)
-appbuilder = AppBuilder(app, security_manager_class=SecurityManager)
-
+            app = Flask(__name__)
+            app.config.from_object('config')
+            app.config['MONGODB_SETTINGS'] = {'HOST':'mongodb://careful:123456abc@ds127771.mlab.com:27771/careful','DB': 'careful'}
+            db = MongoEngine(app)
+            appbuilder = AppBuilder(app, security_manager_class=SecurityManager)
+        else:
+            App.instance.val = arg
 
 """
 from sqlalchemy.engine import Engine
@@ -30,5 +40,5 @@ def set_sqlite_pragma(dbapi_connection, connection_record):
     cursor.execute("PRAGMA foreign_keys=ON")
     cursor.close()
 """    
-
+start = App('init')
 from app import views
