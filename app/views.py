5a883355c372e1eeabc03412ee2dab557bbc43ad
Kor 0 <40504329+kor0@users.noreply.github.com>
2018-07-05 16:01:01 -0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -38,19 +38,19 @@ class T4(ModelView):
     datamodel = MongoEngineInterface(Languages)
 appbuilder.add_view(T4, "T4",category = "To")
 
-class T3(ModelView):
-    datamodel = MongoEngineInterface(Stack)
-appbuilder.add_view(T3, "T3",category = "To")
+#class T3(ModelView):
+ #   datamodel = MongoEngineInterface(Stack)
+#appbuilder.add_view(T3, "T3",category = "To")
 
 
-class T2(ModelView):
-    datamodel = MongoEngineInterface(Gender)
-appbuilder.add_view(T2, "T2",category = "To")
+#class T2(ModelView):
+ #   datamodel = MongoEngineInterface(Gender)
+#appbuilder.add_view(T2, "T2",category = "To")
 
 #
-class T1(ModelView):
-    datamodel = MongoEngineInterface(Action)
-appbuilder.add_view(T1, "T1",category = "To")
+#class T1(ModelView):
+ #   datamodel = MongoEngineInterface(Action)
+#appbuilder.add_view(T1, "T1",category = "To")
 #
 
 #appbuilder.security_cleanup()
