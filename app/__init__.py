637b8f845b5a985e133b50175f356428a1f2646e
Kor 0 <40504329+kor0@users.noreply.github.com>
2018-07-05 15:24:38 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -13,6 +13,7 @@ logging.getLogger().setLevel(logging.DEBUG)
 
 app = Flask(__name__)
 app.config.from_object('config')
+app.config['MONGODB_SETTINGS'] = {'HOST':os.environ.get('MONGOLAB_URI'),'DB': 'FlaskLogin'}
 db = MongoEngine(app)
 appbuilder = AppBuilder(app, security_manager_class=SecurityManager)
 
